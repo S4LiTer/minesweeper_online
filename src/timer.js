3c18aeda64d2ac64ce004fae69b3accbb815c11d
Rozblesk <rozblesk@seznam.cz>
2023-04-18 15:35:40 +0200

    functions for timer

---
+++ src/timer.js
@@ -0,0 +1,25 @@
+var timeInSecondsDevidedByHundret=-1;
+var timerEndedBool=false;
+var startTime;
+
+function StartTimer(){
+    timeInSecondsDevidedByHundret=-1;
+    timerEndedBool=false;
+    startTime = Date.now()
+    Timer();
+}
+async function Timer(){
+    const element = document.getElementById("Timer");
+    timeInSecondsDevidedByHundret=timeInSecondsDevidedByHundret+1;
+    element.innerHTML = timeInSecondsDevidedByHundret;
+    if(timerEndedBool){
+        return 0;
+    }
+    await new Promise(r => setTimeout(r,1000));
+    Timer();
+}
+
+function StopTimer(){
+    timerEndedBool=1;
+    return Date.now()-startTime;
+}
