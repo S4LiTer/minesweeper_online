c8b58eb4ecc9ae7d06b76a63dd95df4d35705dbe
S4LiTer <vojtakodat@gmail.com>
2023-04-21 10:40:58 +0200

    not working

--- src/hint.js
+++ src/hint.js
@@ -1,9 +1,12 @@
+var depth = 0;
+
 class minesweeperAI {
 constructor(height, width) {
     this.generate_field(height, width);
 }
 
 generate_field(height, width) {
+    this.max_depth = 0;
     this.height = height;
     this.width = width;
     this.covered_minefield = [];
@@ -34,11 +37,12 @@ uncover(x, y, value) {
 
 
 
-predict(x, y) {
-
+predict() {
+    this.valid_iterations = 0;
     let local_mine_field = this.copy_matrix(this.covered_minefield);
-    console.log(this.collapse(0, 0, local_mine_field))
+    console.log(this.collapse(local_mine_field, [[[0, 0], [1, 0], [2, 0], [2, 1], [2, 2]], 2]))
     console.log(local_mine_field);
+    console.log(this.valid_iterations);
     /*for(let y = 0; y < this.height; y++ ) {
         for(let x = 0; x < this.width; x++ ) {
             if(this.get_squares_around(x, y, this.covered_minefield, 8) == 0) {
@@ -56,33 +60,90 @@ predict(x, y) {
     }*/
 }
 
+collapse(local_mine_field, collapse_array) {
 
+    let mines_to_place = collapse_array[1];
+
+    collapse_array[0].forEach(coords => {
+        if(local_mine_field[coords[1]][coords[0]] == 9) {
+            mines_to_place--;
+        }
+    });
+
+    if(mines_to_place == 0) {
+        return;}
+
+    else {
+        console.log("mine is beeing placed by:", collapse_array[1]);
+        console.log(this.copy_matrix(local_mine_field));
+    }
+            
+
+        
+    for(let coords_index = 0; coords_index < collapse_array[0].length; coords_index++) {
+        let x = collapse_array[0][coords_index][0];
+        let y = collapse_array[0][coords_index][1];
+
+        if(local_mine_field[y][x] != 10 || mines_to_place < 1){
+            continue;
+        }
+            
+        local_mine_field[y][x] = 9;
+        mines_to_place--;
+
+        console.log("result: ");
+        console.log(this.copy_matrix(local_mine_field));
+
+        if(mines_to_place > 0)
+            {this.collapse(local_mine_field, collapse_array);}
+
+        if(!this.valid(local_mine_field)) {
+            local_mine_field[y][x] = 10;
+            mines_to_place++;
+        }
+        else {
+            console.log("valid result");
+            local_mine_field[y][x] = 10;
+            mines_to_place++;
+            this.valid_iterations++;
+        }
+            
+    }
+}
+
+valid(mine_field_to_verify) {
+    var is_valid = true;
+
+    for(let y = 0; y < mine_field_to_verify.length; y++) {
+        for(let x = 0; x < mine_field_to_verify[0].length; x++) {
+            let square_value = mine_field_to_verify[y][x];
+
+            if(square_value >= 9)
+                continue;
+
+            if(square_value != this.get_squares_around(x, y, mine_field_to_verify, 9).length) {
+                is_valid = false;
+                break;
+            }
+
+        }
+    }
+    return is_valid;
+}
+
+/*
 collapse(x, y, local_mine_field, value_to_test = 9) {
+    depth++;
     var valid = true;
     var valid_iterations_count = 0;
     local_mine_field[y][x] = value_to_test;
-    console.log("trying", x, y, "as", value_to_test);
-    this.copy_matrix(local_mine_field).forEach(a => {
-        var _a = []
-        a.forEach(element => {
-            if(element < 10) {
-                element = `0${element}`;
-                _a.push(element)
-                return;
-            }
-            element = `${element}`;
-            _a.push(element)
-        });
-
-        console.log(_a);
-    });
     
-
+    var coll = true;
     let number_around = this.get_squares_around(x, y, local_mine_field, 8);
     number_around.forEach(coords => {
         if(valid) {
-            valid_iterations_count = this.verify_numbers_around(local_mine_field, coords[0], coords[1]);
-
+            valid_iterations_count = this.verify_numbers_around(local_mine_field, coords[0], coords[1], true);
+            coll = false;
             if(valid_iterations_count == 0) {
                 valid = false;
             }
@@ -90,16 +151,19 @@ collapse(x, y, local_mine_field, value_to_test = 9) {
 
         
     });
+
+    if(depth > this.max_depth && valid) {
+        this.max_depth = depth
+        console.log(valid_iterations_count);
+        console.log(this.copy_matrix(local_mine_field));
+    }
+
+    depth--;
     local_mine_field[y][x] = 10;
-    console.log(valid_iterations_count);
     return valid_iterations_count;
 }
-
-
-verify_numbers_around(local_mine_field, x, y) {
+verify_numbers_around(local_mine_field, x, y, collapse_safe_tiles = false) {
     let mines_around = this.get_squares_around(x, y, local_mine_field, 9).length
-
-    console.log("mines around:", mines_around, " number:", local_mine_field[y][x]);
 
     if(mines_around < local_mine_field[y][x]){
         let uncovered_around = this.get_squares_around(x, y, local_mine_field, 10)
@@ -113,16 +177,13 @@ verify_numbers_around(local_mine_field, x, y) {
     }
 
     if(mines_around == local_mine_field[y][x]) {
-
-        let uncovered_around = this.get_squares_around(x, y, local_mine_field, 10)
-        let valid_iterations_count = 1;
-
-        uncovered_around.forEach(coords => {
-            if(valid_iterations_count != 0)
-                valid_iterations_count = this.collapse(coords[0], coords[1], local_mine_field, 11);
+        //console.log("mines around:", mines_around, " number:", local_mine_field[y][x], "(",x, y,")");
+        let uncovered_around = this.get_squares_around(x, y, local_mine_field, 10);
 
 
-        });
+        if(uncovered_around.length > 0 && collapse_safe_tiles)
+            var valid_iterations_count = this.collapse(uncovered_around[0][0], uncovered_around[0][1], local_mine_field, 11);
+
         if(valid_iterations_count != 0)
             return 1;
 
@@ -130,12 +191,10 @@ verify_numbers_around(local_mine_field, x, y) {
     }
 
     if(mines_around > local_mine_field[y][x]){
-        console.log("nonono");
         return 0;
     }
 }
-
-
+*/
 copy_matrix(matrix_to_copy) {
     var copy = []
 
